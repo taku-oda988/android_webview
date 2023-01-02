c6ff95bc7adb624fe5e7dc7f5c9bfa5978884dd8
takuya oda <ooodddd18@gmail.com>
2023-01-02 21:21:04 +0900

    特定ホストのみ開くように変更

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.webview
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
@@ -8,6 +9,9 @@ import android.view.KeyEvent
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val HOST = "www.google.com"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,12 +22,11 @@ class MainActivity : AppCompatActivity() {
         // Javascriptの有効可（動画再生のため）
         myWebView.settings.javaScriptEnabled = true
         // リンククリック時に他のアプリを起動させない
-        myWebView.webViewClient = WebViewClient()
+        myWebView.webViewClient = MyWebViewClient()
         
         // 起動時に表示するページ
-        myWebView.loadUrl("https://www.youtube.com")
+        myWebView.loadUrl("https://$HOST")
     }
-    
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         val myWebView: WebView = findViewById(R.id.webview)
         // Check if the key event was the Back button and if there's history
@@ -34,5 +37,15 @@ class MainActivity : AppCompatActivity() {
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event)
+    }
+}
+private class MyWebViewClient : WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        if (Uri.parse(url).host == MainActivity.HOST) {
+            // This is my web site, so do not override; let my WebView load the page
+            return false
+        }
+        return true
     }
 }
